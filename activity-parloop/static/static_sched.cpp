fac2a7c3b348561bd40b8ed39dceaa1ac4a247e4
supreethub <supreethub96@gmail.com>
2021-04-24 15:26:41 -0400

    before thread implementation

--- activity-parloop/static/static_sched.cpp
+++ activity-parloop/static/static_sched.cpp
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <chrono>
 #include <cmath>
+#include <vector>
+#include "seq_loop.hpp"
+
+using namespace std;
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,5 +30,52 @@ int main (int argc, char* argv[]) {
     return -1;
   }
   
+  int functionid = stoi(argv[1]);
+  float a = stoi(argv[2]);
+  float b = stoi(argv[3]);
+  float n = stoi(argv[4]);
+  float intensity = stoi(argv[5]);
+  int nbthreads = stoi(argv[6]);
+  int common_expression = ((b - a) / n);
+  float sum = 0.0;
+  std::chrono::time_point<std::chrono::system_clock> begin = std::chrono::system_clock::now();
+
+  SeqLoop s1;
+
+  s1.parfor<float>(
+      0, n, 1, nbthreads,
+      [&](float &tls) -> void {
+        tls = 0.0;
+      },
+      [&](int i, float &tls) -> void {
+        float x = (a + (i + 0.5) * ((b - a) / n));
+        switch (functionid)
+        {
+        case 1:
+          tls += f1(x, intensity);
+          break;
+        case 2:
+          tls += f2(x, intensity);
+          break;
+        case 3:
+          tls += f3(x, intensity);
+          break;
+        case 4:
+          tls += f4(x, intensity);
+          break;
+        }
+      },
+      [&](float &tls) -> void {
+        sum += tls;
+      });
+
+  std::cout << ((b - a) / n) * sum << endl;
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - begin;
+
+  std::cerr << elapsed_seconds.count() << std::endl;
+
+
   return 0;
 }
