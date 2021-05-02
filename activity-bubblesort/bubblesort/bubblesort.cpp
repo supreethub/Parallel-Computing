61d783bec89a1daf15e1e9e84f1730512f815d9f
supreethub <supreethub96@gmail.com>
2021-05-01 20:19:09 -0400

    bubblesort static distribution

--- activity-bubblesort/bubblesort/bubblesort.cpp
+++ activity-bubblesort/bubblesort/bubblesort.cpp
@@ -4,6 +4,12 @@
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
+#include <cmath>
+#include "para_loop.hpp"
+
+
+using namespace std;
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,6 +22,21 @@ extern "C" {
 }
 #endif
 
+void swap(int *arr, int i, int j)
+{
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+void printArr(int *arr, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << "\t";
+  }
+  cout << endl;
+}
 
 int main (int argc, char* argv[]) {
   if (argc < 3) { std::cerr<<"usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
@@ -23,16 +44,47 @@ int main (int argc, char* argv[]) {
   }
 
   int n = atoi(argv[1]);
-  
+  int nbthreads = atoi(argv[2]);
+  // ParaLoop pl;
   // get arr data
   int * arr = new int [n];
   generateMergeSortData (arr, n);
 
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  //test code
+  // printArr(arr, n)
+
   //insert sorting code here.
+  bool flag = true;
+  while(flag)
+  {
+    flag = false;
+    staticFor(1, n-1, 2, nbthreads,
+    [&](int i) -> void {
+      if (arr[i] > arr[i+1])
+      {
+        swap(arr, i, i+1);
+        flag = true;
+      }
+    });
 
+  staticFor( 0, n-1, 2, nbthreads,
+    [&](int i) -> void {
+      if (arr[i] > arr[i+1])
+      {
+        swap(arr, i, i+1);
+        flag = true;
+      }
+    });
+    
+  }
 
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end - start;
   
   checkMergeSortResult (arr, n);
+
+  std::cerr << elpased_seconds.count() << std::endl;
   
   delete[] arr;
 
---
+++ activity-bubblesort/bubblesort/para_loop.hpp
@@ -0,0 +1,103 @@
+#ifndef __PARA_LOOP_H
+#define __PARA_LOOP_H
+
+#include <vector>
+#include <thread>
+#include <functional>
+
+using namespace std;
+
+// class ParaLoop {
+// public:
+  /// @brief execute the function f multiple times with different
+  /// parameters possibly in parallel
+  ///
+  /// f will be executed multiple times with parameters starting at
+  /// beg, no greater than end, in inc increment. These execution may
+  /// be in parallel
+  void parafor (size_t beg, size_t end, size_t inc,
+	       std::function<void(int)> f) {
+    for (size_t i=beg; i<end; i+= inc) {
+      f(i);
+    }
+  }
+
+  /// @brief execute the function f multiple times with different
+  /// parameters possibly in parallel
+  ///
+  /// f will be executed multiple times with parameters starting at
+  /// beg, no greater than end, in inc increment. These execution may
+  /// be in parallel.
+  ///
+  /// Each thread that participate in the calculation will have its
+  /// own TLS object.
+  ///
+  /// Each thread will execute function before prior to any
+  /// calculation on a TLS object.
+  ///
+  /// Each thread will be passed the TLS object it executed before on.
+  ///
+  /// Once the iterations are complete, each thread will execute after
+  /// on the TLS object. No two thread can execute after at the same time.
+  
+  
+
+// void simpleFor(size_t beg, size_t end, size_t inc,
+//               std::function<void(int)> f)
+// {
+//   for (size_t i = beg; i < end; i += inc)
+//   {
+//     f(i);
+//   }
+// }
+
+void staticFor(size_t beg, size_t end, size_t inc, size_t nthreads,
+               std::function<void(int)> func)
+{
+  vector<thread> thread_stack;
+  for (int t = 1, start = beg; t <= nthreads; t++, start += inc)
+  {
+    thread_stack.push_back(thread(parafor, start, end, inc * nthreads, func));
+  }
+  for (auto &itr : thread_stack)
+  {
+    itr.join();
+  }
+}
+  
+  template<typename TLS> 
+   
+  void parfor (size_t beg, size_t end, size_t increment, size_t numthreads,
+	       std::function<void(TLS&)> before,
+	       std::function<void(int, TLS &)> f,
+	       std::function<void(TLS&)> after
+	       )
+    {
+
+    vector<thread> threads;
+    vector<TLS> thrTLS(numthreads);
+
+
+    for (int t = 0; t<numthreads; t+=1)
+    {
+      before(thrTLS[t]);
+      threads.push_back(thread(simpleFor, t, end, numthreads, [&, t](int j) -> void {
+        f(j, thrTLS[t]);
+      }));
+    }
+    
+
+      for (auto &t : threads)
+      {
+        t.join();
+      }
+
+      for (auto &t : thrTLS)
+      {
+        after(t);
+      }
+  }
+  
+// };
+
+#endif
